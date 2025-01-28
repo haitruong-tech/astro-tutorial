291f8797ed7a8eedad7052d39d09ff9531553c7e
Hai Truong <haitruong.tech@gmail.com>
2025-01-28 11:57:29 +0700

    feat: add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://splendid-begonia-2079a3.netlify.app",
+});
