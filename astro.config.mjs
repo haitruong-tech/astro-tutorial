121e02829de8cfce302c6308dafe0349eba8622b
Hai Truong <haitruong.tech@gmail.com>
2025-01-28 12:12:23 +0700

    feat: integrate preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://splendid-begonia-2079a3.netlify.app",
+  integrations: [preact()],
 });
